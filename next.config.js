09bc4f938e493e0b97fa3aed626dcb49b332cd7b
saiko.life2020@gmail.com <saiko.life2020@gmail.com>
2022-07-13 14:35:05 +0900

    remove before add next.config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  source: 'https://firstnextjsapi.herokuapp.com/',
-  destination: 'https://next-sigma-cyan.vercel.app/',
 }
 
 module.exports = nextConfig
