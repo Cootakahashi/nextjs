3899337f1af87eff51ac0be988f69222a7bad9a8
saiko.life2020@gmail.com <saiko.life2020@gmail.com>
2022-07-13 12:45:56 +0900

    added next js config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  source: 'https://firstnextjsapi.herokuapp.com/',
+  destination: 'https://next-sigma-cyan.vercel.app/',
 }
 
 module.exports = nextConfig
